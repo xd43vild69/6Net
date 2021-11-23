8e27a24b64b210033626aae522f8e44aa91adf5a
xd43vild69 <xd43vild69@gmail.com>
2021-11-23 17:04:54 -0500

    add design patterns examples.

---
+++ Net6BaseLib/designPatterns/strategy/StrategyManager.cs
@@ -0,0 +1,74 @@
+namespace Net6BaseLib.designPatterns;
+
+public class StrategyManager
+    {
+        public StrategyManager()
+        {
+            Context context;
+            // Three contexts following different strategies
+            context = new Context(new ConcreteStrategyA());
+            context.ContextInterface();
+            context = new Context(new ConcreteStrategyB());
+            context.ContextInterface();
+            context = new Context(new ConcreteStrategyC());
+            context.ContextInterface();
+            // Wait for user
+            Console.ReadKey();
+        }
+    }
+    /// <summary>
+    /// The 'Strategy' abstract class
+    /// </summary>
+    public abstract class Strategy
+    {
+        public abstract void AlgorithmInterface();
+    }
+    /// <summary>
+    /// A 'ConcreteStrategy' class
+    /// </summary>
+    public class ConcreteStrategyA : Strategy
+    {
+        public override void AlgorithmInterface()
+        {
+            Console.WriteLine(
+                "Called ConcreteStrategyA.AlgorithmInterface()");
+        }
+    }
+    /// <summary>
+    /// A 'ConcreteStrategy' class
+    /// </summary>
+    public class ConcreteStrategyB : Strategy
+    {
+        public override void AlgorithmInterface()
+        {
+            Console.WriteLine(
+                "Called ConcreteStrategyB.AlgorithmInterface()");
+        }
+    }
+    /// <summary>
+    /// A 'ConcreteStrategy' class
+    /// </summary>
+    public class ConcreteStrategyC : Strategy
+    {
+        public override void AlgorithmInterface()
+        {
+            Console.WriteLine(
+                "Called ConcreteStrategyC.AlgorithmInterface()");
+        }
+    }
+    /// <summary>
+    /// The 'Context' class
+    /// </summary>
+    public class Context
+    {
+        Strategy strategy;
+        // Constructor
+        public Context(Strategy strategy)
+        {
+            this.strategy = strategy;
+        }
+        public void ContextInterface()
+        {
+            strategy.AlgorithmInterface();
+        }
+    }
