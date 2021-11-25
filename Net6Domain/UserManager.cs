8e8522b15889fe49e72bae6a1367ca966f3af813
xd43vild69 <xd43vild69@gmail.com>
2021-11-25 12:36:22 -0500

    complete db test.

---
+++ Net6Domain/UserManager.cs
@@ -0,0 +1,13 @@
+﻿using Net6Data;
+using Net6DTO;
+
+namespace Net6Domain;
+public class UserManager
+{
+    public void GetUser()
+    {
+        Net6Data.UserRepository<User> r = new Net6Data.UserRepository<User>();
+        var u = r.GetById(1);
+        string x = string.Empty;
+    }
+}
